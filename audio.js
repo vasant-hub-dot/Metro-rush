50f9336a97a973c9a22261a0504fff849037bfa0
vasant-hub-dot <vasant.beit@gmail.com>
2026-05-09 17:06:30 +0530

    Add playMusic function to manage audio playback

---
+++ audio.js
@@ -0,0 +1,21 @@
+let currentMusic;
+
+export function playMusic(trackName)
+{
+    if(currentMusic)
+    {
+        currentMusic.pause();
+    }
+
+    currentMusic = new Audio(
+
+        `assets/music/${trackName}.mp3`
+
+    );
+
+    currentMusic.loop = true;
+
+    currentMusic.volume = 0.5;
+
+    currentMusic.play();
+}
