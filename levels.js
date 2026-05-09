0375768ce9427c05f6a9cdc822bf44c809d192a3
vasant-hub-dot <vasant.beit@gmail.com>
2026-05-09 17:05:57 +0530

    Add levels configuration for game

---
+++ levels.js
@@ -0,0 +1,57 @@
+export const levels = [
+
+    {
+        level: 1,
+        speed: 0.5,
+        theme: "city"
+    },
+
+    {
+        level: 5,
+        speed: 0.7,
+        theme: "metro"
+    },
+
+    {
+        level: 10,
+        speed: 0.9,
+        theme: "neon"
+    },
+
+    {
+        level: 15,
+        speed: 1.1,
+        theme: "moving-obstacles"
+    },
+
+    {
+        level: 20,
+        speed: 1.3,
+        theme: "night"
+    },
+
+    {
+        level: 25,
+        speed: 1.5,
+        theme: "laser"
+    },
+
+    {
+        level: 30,
+        speed: 1.7,
+        theme: "storm"
+    },
+
+    {
+        level: 35,
+        speed: 1.9,
+        theme: "boss-chase"
+    },
+
+    {
+        level: 40,
+        speed: 2.2,
+        theme: "snow-thunder"
+    }
+
+];
