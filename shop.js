206f2a06d139af772886f2b503ea9f14e794943b
vasant-hub-dot <vasant.beit@gmail.com>
2026-05-16 22:13:05 +0530

    Initialize shopItems with characters and hoverboards
    Added shopItems object with characters, hoverboards, and rides.

---
+++ shop.js
@@ -0,0 +1,134 @@
+export const shopItems = {
+
+characters: [
+
+{
+name: 'Default Runner',
+price: 0,
+unlocked: true,
+color: 0x00ffcc
+},
+
+{
+name: 'Ninja',
+price: 100,
+unlocked: false,
+color: 0x111111
+},
+
+{
+name: 'Robot',
+price: 300,
+unlocked: false,
+color: 0xaaaaaa
+},
+
+{
+name: 'Fire Warrior',
+price: 1000,
+unlocked: false,
+color: 0xff3300
+},
+
+{
+name: 'Ice Queen',
+price: 3000,
+unlocked: false,
+color: 0x66ccff
+},
+
+{
+name: 'Shadow Assassin',
+price: 7000,
+unlocked: false,
+color: 0x220022
+},
+
+{
+name: 'Galaxy Runner',
+price: 10000,
+unlocked: false,
+color: 0x9900ff
+},
+
+{
+name: 'Golden God',
+price: 50000,
+unlocked: false,
+color: 0xffd700
+}
+
+],
+
+
+
+hoverboards: [
+
+{
+name: 'Neon Board',
+price: 200,
+unlocked: false,
+color: 0xff00ff
+},
+
+{
+name: 'Ice Board',
+price: 800,
+unlocked: false,
+color: 0x66ccff
+},
+
+{
+name: 'Lava Board',
+price: 2000,
+unlocked: false,
+color: 0xff2200
+},
+
+{
+name: 'Lightning Board',
+price: 5000,
+unlocked: false,
+color: 0xffff00
+},
+
+{
+name: 'Galaxy Board',
+price: 15000,
+unlocked: false,
+color: 0x6600ff
+}
+
+],
+
+
+
+rides: [
+
+{
+name: 'Mini Plane',
+price: 1500,
+unlocked: false
+},
+
+{
+name: 'Rocket Ride',
+price: 4000,
+unlocked: false
+},
+
+{
+name: 'Dragon Ride',
+price: 10000,
+unlocked: false
+},
+
+{
+name: 'UFO Ride',
+price: 25000,
+unlocked: false
+}
+
+]
+
+};
